d35c414ac1203a3845faecb034280979009e5a72
zj <13552960783@163.com>
2018-07-18 17:03:20 +0800

    init

---
+++ src/main/java/spring/springboot2/x/zj/HelloWorld.java
@@ -0,0 +1,15 @@
+package spring.springboot2.x.zj;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/")
+    public  String HellorWorld(){
+        return  "Hello World Spring boot2.x";
+    }
+}
