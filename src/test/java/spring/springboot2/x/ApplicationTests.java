88cfefd5d533540835c14117e7307f28a39a19e4
zj <13552960783@163.com>
2018-07-19 18:16:46 +0800

    package

--- src/test/java/spring/springboot2/x/ApplicationTests.java
+++ src/test/java/spring/springboot2/x/ApplicationTests.java
@@ -5,12 +5,24 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
+        int totalSecs=12000;
+        int  hours = totalSecs / 3600;
+        int minutes = (totalSecs % 3600) / 60;
+        int seconds = totalSecs % 60;
+        String timeString="";
+        if (hours>0) {
+            timeString = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+        }else {
+            timeString = String.format("%02d:%02d", minutes, seconds);
+        }
+        System.out.println(timeString);
 	}
+
 
 }
