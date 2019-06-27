bda70f4b6ebddbc56085f8fcdfb0438fadf39150
zj <13552960783@163.com>
2019-06-27 11:01:18 +0800

    jpa

--- src/main/java/spring/springboot2/repository/UserBotRepository.java
+++ src/main/java/spring/springboot2/repository/UserBotRepository.java
@@ -1,11 +1,12 @@
 package spring.springboot2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import spring.springboot2.entity.UserBot;
 
 /**
  * @author : ZJ
  * @date : 19-6-21 下午3:58
  */
-public interface UserBotRepository extends JpaRepository<UserBot,Integer> {
+public interface UserBotRepository extends JpaRepository<UserBot,Integer>, JpaSpecificationExecutor {
 }
