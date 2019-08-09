84f679e0d0e880c0ec92ec5c9e8c468a805918b9
zj <13552960783@163.com>
2019-08-09 18:04:49 +0800

    html

---
+++ src/main/java/spring/springboot2/service/ThymeleafService.java
@@ -0,0 +1,11 @@
+package spring.springboot2.service;
+
+/**
+ * @author : ZJ
+ * @date : 19-8-9 上午11:08
+ */
+public interface ThymeleafService {
+    void createHtml(Long id);
+
+    void deleteHtml(Long id);
+}
--- src/main/java/spring/springboot2/zj/HelloWorld.java
+++ src/main/java/spring/springboot2/zj/HelloWorld.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import spring.springboot2.config.WxTest;
+import spring.springboot2.service.ThymeleafService;
 
 @RestController
 public class HelloWorld {
@@ -15,10 +16,26 @@ public class HelloWorld {
     @Autowired
     private WxTest wxTest;
 
+
+    @Autowired
+    ThymeleafService thymeleafService;
+
     @Value(value = "${username}")
     private String name;
+
+
     @GetMapping("/")
-    public  String HellorWorld(){
-        return  "Hello World 1Spring boot2.x for" + name+"==="+wxTest;
+    public String HellorWorld() {
+        return "Hello World 1Spring boot2.x for" + name + "===" + wxTest;
+    }
+
+
+    @GetMapping("/test22")
+    public String HellorWorld11() {
+
+//http://www.glxxw2018.com/study/blog/detail/ag6Gf13MuC.html
+        thymeleafService.createHtml(55L);
+
+        return "ok";
     }
 }
