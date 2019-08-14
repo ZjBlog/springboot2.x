5353d206bb5acd1d57369dbdba179278571aa1ed
zhangjunTracy <13552960783@163.com>
2019-08-14 22:52:07 +0800

    java

--- src/test/java/spring/springboot2/Test5.java
+++ src/test/java/spring/springboot2/Test5.java
@@ -1,25 +1,118 @@
 package spring.springboot2;
 
+import java.util.List;
+
 public class Test5 {
 
-    private static int dosomething(Integer i){
+    private static int dosomething(Integer i) {
         try {
 
             i++;
-            if(i/0>0){
+            if (i / 0 > 0) {
                 i++;
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             i++;
         }
         i++;
 
-        return  i++;
+        return i++;
+    }
+
+    public static void main11(String[] args) {
+        System.out.println(dosomething(10));
+
+
+        /**
+         * +0.5后向下取整。所以结果是-2。
+         */
+        System.out.println(Math.round(-2.7));
+        System.out.println(Math.round(2.6));
+
+        boolean fa = false;
+        long f = 12;
+        System.out.println(f + "");
     }
 
     public static void main(String[] args) {
-        System.out.print( dosomething(10));
+
+
+        System.out.println(count1(11));
+
+
+    }
+
+    /**
+     * 厄拉多塞筛法
+     * @param x
+     * @return
+     */
+    private static Integer count1(int x) {
+
+        boolean[] arr = new boolean[x + 1];
+        // 初始化数组为 true
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = true;
+        }
+
+        for (int k = 2; k< x; k++) {
+            if (arr[k]) {
+                for (int i = k; i * k < x; i++) {
+                    arr[k * i] = false; // k * i is not prime
+                }
+            }
+        }
+
+        int count = 0;
+        for (int j = 2; j < x; j++) {
+            if (arr[j]) {
+                count++;
+            }
+        }
+        return count;
+
+    }
+
+    private static Integer count(int i) {
+        if (i < 2) {
+            return 0;
+        }
+        if (i == 2) {
+            return 1;
+        }
+        Integer count = 1;
+        for (int k = 2; k < i; k++) {
+
+            if (isPrime(k)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static Boolean isPrime(int i) {
+
+
+        for (Integer j = 2; j < (i / 2) + 1; j++) {
+            if (i % j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * 不常出现字符串 空格分隔，
+     * @return
+     */
+    private List<String> str(){
+        String A="apple apple"; String B="banana"; // [banana]
+
+        String C="this is dog";String D="this is cat"; //[dog,cat]
+
+
     }
 
 }
