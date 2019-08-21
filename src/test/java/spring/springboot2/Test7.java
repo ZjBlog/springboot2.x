6450d87fdc80c008c58f3cf3b7217e2cfac7b0bc
zj <13552960783@163.com>
2019-08-21 13:53:59 +0800

    java

--- src/test/java/spring/springboot2/Test7.java
+++ src/test/java/spring/springboot2/Test7.java
@@ -1,6 +1,10 @@
 package spring.springboot2;
 
+import org.junit.Test;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 /**
@@ -9,15 +13,95 @@ import java.util.concurrent.*;
  */
 public class Test7 {
 
-    public static void main(String[] args) {
+    @Test
+    public void test() throws InterruptedException, ExecutionException {
+
+        Executor executor = Executors.newFixedThreadPool(10);
+
+        CompletionService<String> service = new ExecutorCompletionService<>(executor);
+
+        for (int i = 0 ; i < 50 ;i++) {
+            int seqNo = i;
+            service.submit(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    return "HelloWorld-" + seqNo + "-" + Thread.currentThread().getName();
+                }
+            });
+        }
+        for (int j = 0 ; j < 50; j++) {
+            System.out.println(service.take().get());
+        }
+    }
+
+   // @Test
+    public void test7(){
+        /**
+         * 单线程化的线程池
+         */
+        ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            singleThreadExecutor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("=======================");
+                    Thread.currentThread().setName("Thread i = " + index);
+                    System.out.println(Thread.currentThread().getName() + " index = " + index);
+                }
+            });
+        }
+     //   singleThreadExecutor.shutdown();
+        System.out.println("on the main thread...");
+
+    }
+
+    public static void main1(String[] args) {
+
         ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 10, 1000, TimeUnit.SECONDS, new LinkedBlockingQueue(100));
+
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 10, 1000, TimeUnit.SECONDS, new LinkedBlockingQueue(100), new ThreadPoolExecutor.DiscardPolicy());
+
+        List<Future<Integer>> resultList = new ArrayList<Future<Integer>>();
+
+        // 创建10个任务并执行
+        for (int i = 0; i < 11; i++) {
+            // 使用ExecutorService执行Callable类型的任务，并将结果保存在future变量中
+            Future<Integer> future = executor.submit(new TaskWithResult());
+            // 将任务执行结果存储到List中
+            resultList.add(future);
 
 
+            Future<Integer> submit = executor.submit(() -> 1);
+            resultList.add(submit);
+        }
+        //executor.shutdown();
+
+
+        for (Future<Integer> fs : resultList) {
+            try {
+                System.out.println(fs.get()); // 打印各个线程（任务）执行的结果
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                executorService.shutdownNow();
+                e.printStackTrace();
+            }
+        }
+
+
+        for (Integer i = 0; i < 100; i++) {
+            executor.execute(() -> System.out.println(Thread.currentThread().getName()+"===="));
+        }
+
+
+System.out.println("========");
 //        File file = new File("/home/zj/zj/node/html");
 //        Long len = getLen(file, 0l);
 //        System.out.println(len);
+
+        executor.shutdown();
     }
 
     private static Long getLen(File file, Long len) {
