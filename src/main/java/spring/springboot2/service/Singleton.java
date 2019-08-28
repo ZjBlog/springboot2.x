0c045c17a2b104b907cdbf61d6777e26c51549ca
zj <13552960783@163.com>
2019-08-28 17:21:56 +0800

    enume

--- src/main/java/spring/springboot2/service/Singleton.java
+++ src/main/java/spring/springboot2/service/Singleton.java
@@ -4,10 +4,19 @@ package spring.springboot2.service;
  * @author : ZJ
  * @date : 19-8-28 下午4:40
  */
+
+/**
+ * enum(string,int)构造方法
+ */
 public enum Singleton {
     INSTANCE;
-    private SingletonClass instance;
 
+    private SingletonClass instance;
+    /**
+     * //构造方法
+     * //但是构造方法的修饰符有限制，不可以是public，只能private
+     * //实际上即使不写private，默认的也是private
+     */
     Singleton() {
         System.out.println("枚举类构造函数");
         this.instance = new SingletonClass();
@@ -21,7 +30,6 @@ public enum Singleton {
 
 class SingletonClass {
     int i = 0;
-
     public SingletonClass() {
         System.out.println("SingletonClass被初始化 " + ++i + " 次");
     }
@@ -32,5 +40,6 @@ class SingletonTest {
         SingletonClass instance1 = Singleton.INSTANCE.getInstance();
         SingletonClass instance2 = Singleton.INSTANCE.getInstance();
         System.out.println("instance1 == instance2: " + (instance1 == instance2));
+        System.out.println(Singleton.INSTANCE.name());
     }
 }
