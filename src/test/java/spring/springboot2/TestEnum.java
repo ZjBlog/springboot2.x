518e38cbb52144cec42c64d9fc3d087f70921c83
zhangjun <zhangjun486@jd.com>
2019-11-18 11:43:51 +0800

    'enum测试';

--- src/test/java/spring/springboot2/TestEnum.java
+++ src/test/java/spring/springboot2/TestEnum.java
@@ -24,30 +24,37 @@ public class TestEnum {
             System.out.println(object.toString());
         }
 
+
+        /**
+         *  仅能获取类本身的 private属性成员
+         */
         Field field = aClass.getDeclaredField("type");
         Field nameField = aClass.getDeclaredField("name");
+
         field.setAccessible(true);
         nameField.setAccessible(true);
 
+        /**
+         * 仅能获取类(及其父类可以自己测试) public属性成员
+         */
+        //aClass.getField("type");
 
+
+
+        System.out.println("=====================");
         for (Object o : objects) {
-
-            System.out.println(o);
-            System.out.println("=====================");
+            //获取属性
             Object o1 = field.get(o);
             if (o1.equals("1024")) {
-                nameField.get(o).toString();
+                nameField.get(o);
                 System.out.println(nameField.get(o).toString());
             }
 
         }
-
+        System.out.println("=====================");
         CouponType[] values = CouponType.values();
         for (CouponType value : values) {
-
             System.out.println(value);
-            System.out.println(value.getType());
-            System.out.println(value.name());
         }
 
     }
