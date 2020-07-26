739fde04455cb3622a289e7d5e3e15daf1c317af
zhangjun486 <zhangjun486@jd.com>
2020-07-26 15:42:23 +0800

    'date'

--- src/main/java/spring/springboot2/Until/DateUntil.java
+++ src/main/java/spring/springboot2/Until/DateUntil.java
@@ -2,11 +2,6 @@ package spring.springboot2.Until;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author : ZJ
@@ -25,16 +20,5 @@ public class DateUntil {
                 return new SimpleDateFormat("yyyy.MM.dd");
             }
     );
-
-    public static void main(String[] args) {
-        Map<String, String> map = new HashMap<>();
-        map.put("ok", "dd");
-        map.put("ok1", "dd");
-        map.put("ok2", "ppp");
-        map.put("ok3", "ppp");
-        map.put("ok4", "ppp");
-        map.put("ok5", "ppp");
-        map.put("ok6", "ppp");
-    }
 
 }
