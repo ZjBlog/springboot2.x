85d1a445dbeff65e16521b35fb45b36ba2ddfa4b
zhangjun486 <zhangjun486@jd.com>
2020-07-30 10:54:10 +0800

    'd'

--- src/main/bin/start.sh
+++ src/main/bin/start.sh
@@ -3,7 +3,8 @@
 #参数简介：
 #    $1:jar文件名（包含后缀名）
 #    注意：jar文件必须位于startup.sh目录的上一层目录。
-
+# $0 脚本名称
+# dirname 返回脚本文件的目录路径
 #启动参数
 JAVA_OPTS="-server -Xms400m -Xmx400m -Xmn300m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m -Xverify:none -XX:+DisableExplicitGC -Djava.awt.headless=true"
 
@@ -13,7 +14,8 @@ parent_dir=`dirname "${this_dir}"`
 log_dir="/export/log/yfk-service"
 log_file="${log_dir}/console.log"
 jar_file="/export/App/yfk-service.wangyin.com/lib/${jar_name}"
-
+# 如果是目录创建
+# -d filename 如果 filename为目录，则为真
 if [ ! -d "log_dir" ];then
   mkdir $log_dir
   touch $log_file
@@ -21,7 +23,7 @@ if [ ! -d "log_dir" ];then
 else
   echo "日志文件已经存在"
 fi
-
+#-f filename 如果 filename为常规文件，则为真
 #父目录下jar文件存在
 if [ -f "${jar_file}" ]; then
     #启动jar包；重定向标准错误输出到文件，丢掉标准输出
