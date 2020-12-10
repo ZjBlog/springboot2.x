91b70c4da22279b5ebfcf588e4d7bf1035f764e6
zhangjun486 <zhangjun486@jd.com>
2020-12-10 11:13:50 +0800

    '工具类'

--- src/main/java/spring/springboot2/Until/DateUntil.java
+++ src/main/java/spring/springboot2/Until/DateUntil.java
@@ -2,6 +2,9 @@ package spring.springboot2.Until;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /**
  * @author : ZJ
@@ -9,16 +12,28 @@ import java.text.SimpleDateFormat;
  */
 public class DateUntil {
 
-    private static ThreadLocal<DateFormat> threadLocal = new ThreadLocal<DateFormat>() {
-        @Override
-        protected DateFormat
-        initialValue() {
-            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        }
-    };
-    private static ThreadLocal<DateFormat> threadLocal2 = ThreadLocal.withInitial(() -> {
-                return new SimpleDateFormat("yyyy.MM.dd");
-            }
-    );
+    private static ThreadLocal<DateFormat> threadLocal =
+        ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+    private static ThreadLocal<DateFormat> threadLocal2 =
+        ThreadLocal.withInitial(() -> new SimpleDateFormat("yyyy.MM.dd"));
+
+    public static String pare() {
+        DateFormat dateFormat = threadLocal2.get();
+        String format = dateFormat.format(new Date());
+        return format;
+    }
+
+    /**
+     * java8使用DateTimeFormatter 格式化
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate now = LocalDate.now();
+        String format = now.format(pattern);
+        System.out.println(format);
+        System.out.println(LocalDate.parse("2020-12-10", pattern));
+    }
 
 }
--- src/test/java/spring/springboot2/Test1/TetsSpel.java
+++ src/test/java/spring/springboot2/Test1/TetsSpel.java
@@ -1,14 +1,19 @@
 package spring.springboot2.Test1;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.time.FastDateFormat;
 import org.springframework.context.expression.MapAccessor;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
-import spring.springboot2.Until.SpelHelper;
 
-import java.util.HashMap;
-import java.util.Map;
+import spring.springboot2.Until.SpelHelper;
 
 /**
  * @author zhangjun486
@@ -18,37 +23,47 @@ import java.util.Map;
  */
 public class TetsSpel {
 
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         StandardEvaluationContext ctx = new StandardEvaluationContext();
         // 属性读取
-        //key value
+        // key value
         ctx.addPropertyAccessor(new MapAccessor());
-
 
         String exp = "new spring.springboot2.Test1.WorkeService().run(param)";
 
         Map<String, Object> root = new HashMap<>();
         root.put("param", "test");
-        //root 执行表达式的对象
+        // root 执行表达式的对象
         // exp 在root中获取参数
         Object result = SpelHelper.eval(exp, ctx, root, true);
 
         System.out.println(result);
 
-
     }
 
-
-    public static void main1(String[] args) {
+    public static void main11(String[] args) {
         StandardEvaluationContext ctx = new StandardEvaluationContext();
         ExpressionParser parser = new SpelExpressionParser();
 
-
         Expression expression = parser.parseExpression("new spring.springboot2.Test1.WorkeService().run()");
         Map<String, Object> map = new HashMap<>();
-        //执行表达式对象map
-        //执行表达式的根对象
+        // 执行表达式对象map
+        // 执行表达式的根对象
         Object value = expression.getValue(ctx, map);
         System.out.println(value);
+    }
+
+    public static void main(String[] args) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate parse = LocalDate.parse("2014-10-22");
+        System.out.println(parse);
+        Date date = new Date();
+        // Calendar calendar = Calendar.getInstance();
+        // calendar.setTime(date);
+        // calendar.add(Calendar.YEAR, 1);
+        // date = calendar.getTime();
+        // System.out.println(date);
+        FastDateFormat sdf = FastDateFormat.getInstance("yyyy-MM-dd");
+        System.out.println(sdf.format(date));
     }
 }
